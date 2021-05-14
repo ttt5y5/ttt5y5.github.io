612c6c38f2ce72b4de38e5d4ebb2727268c042f4
Sunho <ttt5y5@naver.com>
2021-05-14 16:48:52 +0900

    7교시커밋

---
+++ test/json.js
@@ -0,0 +1,122 @@
+var jsonData = [
+    {
+      "_id": "609e1264e31ec21bb0e5c001",
+      "age": 29,
+      "eyeColor": "green",
+      "name": "Lyons Burnett"
+    },
+    {
+      "_id": "609e1264c4714b9924b02632",
+      "age": 35,
+      "eyeColor": "brown",
+      "name": "Gibbs Foreman"
+    },
+    {
+      "_id": "609e1264e39a80ea95a222fa",
+      "age": 30,
+      "eyeColor": "green",
+      "name": "Potts Medina"
+    },
+    {
+      "_id": "609e1264d04d494628881fb4",
+      "age": 27,
+      "eyeColor": "brown",
+      "name": "Neva Weeks"
+    },
+    {
+      "_id": "609e12642294dffd62ee366b",
+      "age": 25,
+      "eyeColor": "blue",
+      "name": "Hayden Kelley"
+    },
+    {
+      "_id": "609e12642d7c4a2b441ac5f0",
+      "age": 36,
+      "eyeColor": "brown",
+      "name": "Buckley Frederick"
+    },
+    {
+      "_id": "609e12648829cc52fab9ffd2",
+      "age": 39,
+      "eyeColor": "blue",
+      "name": "Leonor Wagner"
+    },
+    {
+      "_id": "609e126468d600eb0cca86cf",
+      "age": 25,
+      "eyeColor": "blue",
+      "name": "Lucile Fulton"
+    },
+    {
+      "_id": "609e126402fa0460759f0141",
+      "age": 28,
+      "eyeColor": "brown",
+      "name": "Olsen Griffith"
+    },
+    {
+      "_id": "609e1264870724f5310173fb",
+      "age": 20,
+      "eyeColor": "blue",
+      "name": "Paige Cohen"
+    },
+    {
+      "_id": "609e1264b5e0e791151b6234",
+      "age": 28,
+      "eyeColor": "green",
+      "name": "Elba Fernandez"
+    },
+    {
+      "_id": "609e12643ccf857e23e2e1a9",
+      "age": 28,
+      "eyeColor": "green",
+      "name": "Helga Riddle"
+    },
+    {
+      "_id": "609e12646e072a9695b3e4e5",
+      "age": 20,
+      "eyeColor": "blue",
+      "name": "Chang Stephenson"
+    },
+    {
+      "_id": "609e1264b2dfbc96c2032e64",
+      "age": 31,
+      "eyeColor": "blue",
+      "name": "Christi Lane"
+    },
+    {
+      "_id": "609e1264df0eb9a702a16ffd",
+      "age": 34,
+      "eyeColor": "blue",
+      "name": "Gutierrez Beach"
+    },
+    {
+      "_id": "609e12642c915b4699da5392",
+      "age": 32,
+      "eyeColor": "brown",
+      "name": "Jerri Nixon"
+    },
+    {
+      "_id": "609e1264971da650beb44ec4",
+      "age": 26,
+      "eyeColor": "blue",
+      "name": "Barbara Nunez"
+    },
+    {
+      "_id": "609e12646492962b01cdb241",
+      "age": 40,
+      "eyeColor": "green",
+      "name": "Perry Dillard"
+    },
+    {
+      "_id": "609e12649cf9908ee6ed630a",
+      "age": 32,
+      "eyeColor": "green",
+      "name": "Fuentes Chaney"
+    },
+    {
+      "_id": "609e126408ffbbb2c64edb1b",
+      "age": 20,
+      "eyeColor": "green",
+      "name": "Glass Calderon"
+    }
+  ]
