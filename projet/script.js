b19a9443d9072c2f0aad381c0141bcb9fc142477
Sunho <ttt5y5@naver.com>
2021-06-01 19:05:25 +0900

    0601 1905

---
+++ projet/script.js
@@ -0,0 +1,7 @@
+function forclear() {
+    document.getElementById("output").innerHTML = "0";
+}
+function perc() {
+    var value = document.getElementById("output").innerHTML
+}
+value = value/100
