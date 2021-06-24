20c55784ba75f7545910a6aa9f4207ed6a7923a6
Sunho <ttt5y5@naver.com>
2021-06-24 16:15:00 +0900

    2021 0624 1614

--- project/script.js
+++ project/script.js
@@ -17,9 +17,30 @@ function fordisplay(value) {
     removeZero()
     document.getElementById("output").innerHTML += value;
 }
-function solve() {
+function solve() {// =
      removeZero()
      var equation = document.getElementById("output").innerHTML;
      var solved = eval(equation);
      document.getElementById('output').innerHTML = solved;
+}
+function del() {//한글자 지우기
+    var value = document.getElementById("output").innerHTML;
+    value = value.slice(0,-1);
+    document.getElementById("output").innerHTML = value;
+    if (value == " " || value.length == 0) {
+        value = "0"
+        document.getElementById("output").innerHTML = value;
+    }
+}
+
+function sqrt() {//제곱
+    var value = document.getElementById("output").innerHTML;
+    value = Math.sqrt(value)
+    document.getElementById("output").innerHTML = value ;
+}
+
+function square() {//루트
+    var value = document.getElementById("output").innerHTML;
+    value = value*value
+    document.getElementById("output").innerHTML = value ;
 }
